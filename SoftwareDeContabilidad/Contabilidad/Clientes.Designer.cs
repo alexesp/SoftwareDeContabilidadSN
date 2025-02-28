0f4f57c73e391e66c87ad2f7cca31db7f0ca528b
alexesp <aleizaes@hotmail.com>
2025-02-28 23:31:41 +0100

    Crear datatabla y dataadapter

--- SoftwareDeContabilidad/Contabilidad/Clientes.Designer.cs
+++ SoftwareDeContabilidad/Contabilidad/Clientes.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Clientes";
+            SuspendLayout();
+            // 
+            // Clientes
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Clientes";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Clientes";
+            ResumeLayout(false);
         }
 
         #endregion
